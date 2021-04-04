c71ec34e2df9cbeff28a02fe4b99f2ee8efffb8d
MauroBraga <M@r202150016774>
2021-04-04 14:23:20 -0300

    Reduzindo Ifs

--- modelo.py
+++ modelo.py
@@ -18,8 +18,8 @@ class Programa:
     def nome(self, novo_nome):
         self._nome = novo_nome.title();
 
-    def imprime(self):
-        print(f'{self._nome} - {self.ano} - {self._likes} Likes')
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self._likes} Likes';
 
 class Filme(Programa):
     def __init__(self,nome,ano,duracao):
@@ -29,16 +29,16 @@ class Filme(Programa):
     def retorna_cadastro_diferenciado(self):
         pass
 
-    def imprime(self):
-        print(f'{self._nome} - {self.ano} - {self.duracao} min - {self._likes} Likes')
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.duracao} min - {self._likes} Likes';
 
 class Serie(Programa):
     def __init__(self,nome,ano,temporadas):
         super().__init__(nome, ano);
         self.temporadas=temporadas
 
-    def imprime(self):
-        print(f'{self._nome} - {self.ano} - {self.temporadas} Temporadas - {self._likes} Likes')
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.temporadas} Temporadas - {self._likes} Likes';
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
@@ -50,4 +50,4 @@ atlanta.dar_like()
 filmes_e_series = [vingadores, atlanta];
 
 for programa in filmes_e_series:
-   programa.imprime();
+   print(programa)
